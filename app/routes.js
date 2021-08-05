0f7507a753bff2c2cd6c88561959e5739129fc74
Tom <thomas.brown1@digital.hmrc.gov.uk>
2021-08-05 12:36:25 +0100

    Removed all v2 version

--- app/routes.js
+++ app/routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-require('./views/v2/routes')(router) 
+
 
 module.exports = router
 
