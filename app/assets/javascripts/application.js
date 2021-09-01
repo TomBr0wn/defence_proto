0a7a23df1d9abf3fcb03c8f58d139c3b0438e4ab
Tom <thomas.brown1@digital.hmrc.gov.uk>
2021-09-01 14:47:11 +0100

    Updated all analyse pages to top nav + top sub nav style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,19 +2,25 @@
 
 // Warn about using the kit in production
 if (window.console && window.console.info) {
-    window.console.info("GOV.UK Prototype Kit - do not use for production");
+	window.console.info("GOV.UK Prototype Kit - do not use for production");
 }
 
-let current = new Date();
-let cDate = current.getFullYear() + "/" + (current.getMonth() + 1) + "/" + current.getDate();
-let cTime = current.getHours() + ":" + current.getMinutes();
+//let current = new Date();
+
+let current = new Date().toLocaleDateString("en-us", { weekday: "long", year: "numeric", month: "short", day: "numeric" });
+// "Friday, Jul 2, 2021
+
+//let cDate = current.getFullYear() + "/" + (current.getMonth() + 1) + "/" + current.getDate();
+//let cTime = current.getHours() + ":" + current.getMinutes();
 // let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
-let dateTime = cDate + " " + " " + cTime;
-console.log(dateTime);
+//let dateTime = cDate + " " + " " + cTime;
+//console.log(dateTime);
 
-$(document).ready(function() {
-    window.GOVUKFrontend.initAll();
-    window.MOJFrontend.initAll();
+console.log(current);
 
-    $(".lastUpdateTime").html(dateTime);
+$(document).ready(function () {
+	window.GOVUKFrontend.initAll();
+	window.MOJFrontend.initAll();
+
+	$(".lastUpdateTime").html(current);
 });
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const formData = [];
 
-
 // Add your routes here - above the module.exports line
 
 // Start folder specific routes
@@ -27,22 +26,26 @@ router.post("/v2/create-new-dataset-route", function (req, res) {
 	res.redirect("create-new-dataset");
 });
 
-router.post("/v2/defence-home-add-row", function (req, res) {
-	console.log("res");
-	console.log(req.session.data);
-	// this also works as long as the variable doesn't have a - in the name
-	formData.push(req.session.data);
-  //res.render("defence-home", { results: formData });
-  res.redirect("defence-home");
-});
+router.post(
+	"/v2/defence-home-add-row",
+	function (req, res, next) {
+		console.log("res");
+		console.log(req.session.data);
+		// this also works as long as the variable doesn't have a - in the name
+		formData.push(req.session.data);
+
+		res.redirect("defence-home-filled");
+		
+	
+	});
 
 router.post("/v2/compare-with-another-dataset", function (req, res) {
 	console.log("res");
 	console.log(req.session.data);
 	// this also works as long as the variable doesn't have a - in the name
 	formData.push(req.session.data);
-  //res.render("defence-home", { results: formData });
-  res.redirect("existing-and-comparable-new");
+	//res.render("defence-home", { results: formData });
+	res.redirect("existing-and-comparable-new");
 });
 
 module.exports = router;
