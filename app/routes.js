56f164ca1c565d00b63b8e846629d87577a4eb25
Tom <thomas.brown1@digital.hmrc.gov.uk>
2021-09-13 12:27:04 +0100

    Added 'Create New dataset Workfloe to Prototypr homepage

--- app/routes.js
+++ app/routes.js
@@ -8,8 +8,9 @@ const formData = [];
 // Start folder specific routes
 router.use("/v1", require("./views/v1/_routes"));
 router.use("/v2", require("./views/v2/_routes"));
+router.use("/v3", require("./views/v3/_routes"));
 
-//Create dataset route
+//////////////  Create V2 dataset route   ////////////////////////////////////
 router.post("/v2/create-new-dataset-route", function (req, res) {
 	console.log("Here we go...");
 	console.log(req.session.data);
@@ -26,6 +27,25 @@ router.post("/v2/create-new-dataset-route", function (req, res) {
 	res.redirect("create-new-dataset");
 });
 
+//////////////  Create V3 dataset route   ////////////////////////////////////
+router.post("/v3/create-new-dataset-route", function (req, res) {
+	console.log("Here we go...");
+	console.log(req.session.data);
+	// this also works as long as the variable doesn't have a - in the name
+	formData.push(req.session.data);
+
+	var datasetId = req.session.data["dataset-id"];
+	console.log("Your new " + datasetId);
+
+	console.log(datasetId);
+
+	console.log(formData);
+
+	res.redirect("create-new-dataset");
+});
+
+
+//////////////  Create V2 dataset home new record/row   ////////////////////////////////////
 router.post(
 	"/v2/defence-home-add-row",
 	function (req, res, next) {
@@ -38,8 +58,35 @@ router.post(
 		
 	
 	});
+	
+//////////////  Create V3 dataset home new record/row   ////////////////////////////////////
+router.post(
+	"/v3/defence-home-add-row",
+	function (req, res, next) {
+		console.log("res");
+		console.log(req.session.data);
+		// this also works as long as the variable doesn't have a - in the name
+		formData.push(req.session.data);
 
+		res.redirect("defence-home-filled");
+		
+	
+	});
+
+//////////////  V2 comparable data   ////////////////////////////////////
+	
 router.post("/v2/compare-with-another-dataset", function (req, res) {
+	console.log("res");
+	console.log(req.session.data);
+	// this also works as long as the variable doesn't have a - in the name
+	formData.push(req.session.data);
+	//res.render("defence-home", { results: formData });
+	res.redirect("existing-and-comparable-new");
+});
+
+//////////////  V3 comparable data   ////////////////////////////////////
+	
+router.post("/v3/compare-with-another-dataset", function (req, res) {
 	console.log("res");
 	console.log(req.session.data);
 	// this also works as long as the variable doesn't have a - in the name
