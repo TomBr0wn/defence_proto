347d6ebe89c263686d84c0c2c161ac17a66bb46c
Tom <thomas.brown1@digital.hmrc.gov.uk>
2021-10-15 09:24:56 +0100

    Defence-home + defnce-home-filled change h1 to Settlement groups

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Settlement Protoype',
+  serviceName: 'Prototype',
 
   // Default port that prototype runs on
   port: '3000',
