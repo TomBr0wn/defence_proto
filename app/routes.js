9e8c787f3bc497dd2b1fc90048a67846c8b51928
Tom <thomas.brown1@digital.hmrc.gov.uk>
2022-01-10 11:47:39 +0000

    Added version 6.2

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,9 @@ router.use("/v1", require("./views/v1/_routes"));
 router.use("/v2", require("./views/v2/_routes"));
 router.use("/v3", require("./views/v3/_routes"));
 router.use("/v4", require("./views/v4/_routes"));
+router.use("/v5", require("./views/v5/_routes"));
+router.use("/v6", require("./views/v6/_routes"));
+router.use("/v62", require("./views/v62/_routes"));
 
 //////////////  Create V2 dataset route   ////////////////////////////////////
 router.post("/v2/create-new-dataset-route", function (req, res) {
@@ -288,6 +291,72 @@ router.get("/v6/edit-case-clear-criteria", function (req, res) {
 	req.session.destroy();
 	res.redirect("/v6/edit-settlement-case");
 });
+
+
+//////////////////////////// V62 Stuff ////////////////////////////
+
+// // Run this code when a form is submitted to 'choose-app-answer'
+router.post("/v62/choose-app-answer", function (req, res) {
+	// Make a variable and give it the value from 'pick-service'
+	var whichService = req.session.data["pick-service"];
+
+	// Check whether the variable matches a condition
+	if (whichService == "settlement") {
+		// Send user to next page
+		res.redirect("/v62/defence-home");
+	} else {
+		// Send user to other page
+		res.redirect("/v62/property-groups");
+	}
+	req.session.destroy();
+});
+
+router.post("/v62/create-new-case-route", function (req, res) {
+	console.log("Here we go...");
+	console.log(req.session.data);
+	// this also works as long as the variable doesn't have a - in the name
+	formData.push(req.session.data);
+
+	var createDescription = req.session.data["description"];
+	var createRegValUnit = req.session.data["reg-val-units"];
+
+	var createEconRegion = req.session.data["economic-region"];
+	var createbillAuthCodes = req.session.data["bill-auth-codes"];
+	var createBulkClassInd = req.session.data["bulk-class-indicator"];
+
+	var createSubLocation = req.session.data["sub-location"];
+	var createScatCode = req.session.data["scat-code"];
+	var createScatSuffix = req.session.data["scat-suffix"];
+
+	console.log(formData);
+	res.redirect("create-new-case");
+});
+
+router.post("/v62/add-case-address", function (req, res) {
+	console.log("Here we go...");
+	console.log(req.session.data);
+	// this also works as long as the variable doesn't have a - in the name
+	formData.push(req.session.data);
+
+	var caseAddress = req.session.data["case-address"];
+
+	console.log("Case Address: " + caseAddress);
+
+	if (req.session.data["case-address"] != " ") {
+		res.redirect("add-case-task-list");
+	} else {
+		res.redirect("settlement-task-list");
+	}
+});
+
+router.get("/v62/edit-case-clear-criteria", function (req, res) {
+	req.session.destroy();
+	res.redirect("/v62/edit-settlement-case");
+});
+
+
+
+
 
 ////////// Edit Case /////////////
 
